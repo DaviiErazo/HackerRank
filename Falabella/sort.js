239daf66b990f2110d4e011c23343ae83efc21dd
David <da.erazom@gmail.com>
2021-07-28 10:57:42 -0400

    asdasd

--- Falabella/sort.js
+++ Falabella/sort.js
@@ -1,3 +1,20 @@
+/**
+ * PRODUCT SORT
+ *  In a warehouse, a manager would like to sort the items
+ *  in the stock. Given an array of n item values, sort the 
+ *  array in ascending order, first by the number of items 
+ *  with a certain value, then by the values themselves.
+ * 
+ * Example
+ * n = 6
+ * items = [4,5,6,5,4,3]
+ *  
+ *  - There are 2 values that occur twice: [4, 4, 5, 5].
+ *  - There are 2 values that occur once: [3, 6],
+ *  - The array of items sorted by quantity and then by value in ascernding order is [3, 6, 4, 4, 5, 5]
+ */
+
+
 const numbers = [2, 3, 5, 5, 5, 1, 1, 6, 4, 4, 4, 3, 3, 3, 3];
 let numbersRepeat = [];
 let numbersNotRepeat = [];
